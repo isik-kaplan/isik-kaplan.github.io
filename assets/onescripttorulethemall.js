2c8991352345e9e37920988740d0208ec42ad412
Işık Kaplan <isik.kaplan@hotmail.com>
2019-08-30 19:12:19 +0300

    First Scratch

---
+++ assets/onescripttorulethemall.js
@@ -0,0 +1,13 @@
+let link = document.getElementsByClassName("representation")[0];
+Array.from(document.getElementsByClassName("connections-wrapper")[0].children).forEach(function (item) {
+    item = item.children[0];
+    item.addEventListener(
+            'click',
+            function(event) {
+                event.preventDefault();
+                link.innerHTML = item.getAttribute('data-href');
+                link.target = item.target;
+                link.href = item.href;
+            }
+        )
+});
