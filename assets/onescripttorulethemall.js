84d0aa99716527f814eb022f1970386462f6c0d0
Işık Kaplan <isik.kaplan@hotmail.com>
2019-08-30 21:06:15 +0300

    encapsualte js

--- assets/onescripttorulethemall.js
+++ assets/onescripttorulethemall.js
@@ -1,13 +1,15 @@
-let link = document.getElementsByClassName("representation")[0];
-Array.from(document.getElementsByClassName("connections-wrapper")[0].children).forEach(function (item) {
-    item = item.children[0];
-    item.addEventListener(
+(function () {
+    let link = document.getElementsByClassName("representation")[0];
+    Array.from(document.getElementsByClassName("connections-wrapper")[0].children).forEach(function (item) {
+        item = item.children[0];
+        item.addEventListener(
             'click',
-            function(event) {
+            function (event) {
                 event.preventDefault();
                 link.innerHTML = item.getAttribute('data-href');
                 link.target = item.target;
                 link.href = item.href;
             }
         )
-});
+    });
+}());
