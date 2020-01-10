266dd47c674db22dcfbc0a205a120446267fcf76
Işık Kaplan <isik.kaplan@hotmail.com>
2020-01-10 06:54:46 +0300

    Make the dark theme default

--- assets/scripts/common/initial.js
+++ assets/scripts/common/initial.js
@@ -3,5 +3,7 @@
     let mode = window.localStorage.getItem("mode");
     if (mode === "dark") {
         html.classList.add("dark")
+    } else if (mode === "light") {
+        html.classList.remove("dark")
     }
 }());
